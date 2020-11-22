7a9831b83d21e4bf8df92ae11177b77ce4d3dcac
TJZii <tjhaweszii@gmail.com>
2020-11-22 15:35:51 -0800

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(arr)
+
+        found = []
+
+        arr.each do |possible|
+            arra = possible.split("")
+            drow = @word.split("")
+            if drow.sort == arra.sort
+                found << possible
+            end
+        end
+        return found
+    end
+
+end
